7854640af8b261a16700d5815c3f4a543c745fd9
annaper <pertzovski.anna@gmail.com>
2021-05-22 20:04:43 +0300

    assignment 9

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,8 +17,26 @@ def assignment8():
 def list1():
     return render_template('list.html')
 
+@app.route('/assignment9', methods=['get', 'post'])
+def assignment9():
+    users_list = [{"firstName": "Michael", "lastName": "Lawson", "Email": "michael.lawson@reqres.in"},
+                  {"firstName": "Lindsay", "lastName": "Ferguson", "Email": "indsay.ferguson@reqres.in"},
+                  {"firstName": "Tobias", "lastName": "Funke", "Email": "tobias.funke@reqres.in"},
+                  {"firstName": "Byron", "Ferguson": "Fields", "Email": "byron.fields@reqres.in"},
+                  {"firstName": "George", "lastName": "Edwards", "Email": "george.edwards@reqres.in"},
+                  {"firstName": "Rachel", "Ferguson": "Howell", "Email": "rachel.howell@reqres.in"}]
+    result = ''
+
+    if 'search' in request.args:
+        for user in users_list:
+            if request.args['search'] == user['firstName'] \
+                    or request.args['search'] == user['lastName'] \
+                    or request.args['search'] == user['Email']:
+
+                result = [user['firstName'], user['lastName'], user['Email']]
+    else:
+        result = ''
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
